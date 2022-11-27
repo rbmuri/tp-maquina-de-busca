1a390d38e1ee0b9bec4b38ddb86b3e4d0db0489f
rbmuri <murilo.r.freitas@hotmail.com>
2022-11-27 15:14:54 -0300

    criei func pra transformar file em vector de string

--- README.md
+++
@@ -1,1 +0,0 @@
-# tp-maquina-de-busca
--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <indexacao.h>
 #include <filesystem>
+#include <fstream>
 
 using namespace std;
 
+
+vector<string> filetostr(string dir){
+    vector<string> sfile;
+    string word;
+    fstream file;
+    file.open(dir);
+
+    while (file.good()) {
+        file >> word;
+        sfile.insert(word);
+    }  
+    sfile.pop_back();
+    return sfile;
+}
+
+
 int main()
 {
+    string nome;
+    Indice index;
+    nome = "README.txt";
+
+    filetostr(nome);
+
     
-    cout<<"maquina de busca\n";
+    
+    
+
+    
+
+    /*index.indexar(file, nome);*/
+
     return 0;
 }
