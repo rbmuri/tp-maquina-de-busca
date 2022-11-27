7d7e9b5e7eefd5fc806e45658f78be777b2d595d
Wigane <119106783+Wigane@users.noreply.github.com>
2022-11-27 10:40:08 -0300

    Update indexacao.h

--- include/indexacao.h
+++ include/indexacao.h
@@ -37,10 +37,45 @@ class Indice {
                 return resultado;
         }
         //* Divide as palavras da String.
-
-        string normalizar(string palavra);
-            //* codigo do lucas
-        
-
+    
+        string normalizar(string palavra){
+            string resultado;
+            string errado[79] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"
+                , "J", "K", "L", "M", "N", "O", "P", "Q"
+                , "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "ç", "Ç", "ñ"
+                , "Ñ", "ÿ", "ý", "Ý", "Á", "À", "Â", "Ã", "Ä"
+                , "Å", "å", "ä", "á", "à", "ã", "â", "É", "È", "Ê", "Ë", "é"
+                , "è", "ê", "ë", "Í", "Ì", "Î", "Ï", "í", "ì"
+                , "î", "ï", "Ó", "Ò", "Õ", "Ô", "Ö", "ó", "ò", "õ", "ô", "Ö"
+                , "Ú", "Ù", "Û", "Ü", "ú", "ù", "û", "ü"
+            };
+            string certo[79] = {"a", "b", "c", "d", "e", "f", "g", "h", "i"
+                , "j", "k", "l", "m", "n", "o", "p", "q"
+                , "r", "s", "t", "u", "v", "w", "x", "y", "z", "c", "c", "n"
+                , "n", "y", "y", "y", "a", "a", "a", "a", "a"
+                , "a", "a", "a", "a", "a", "a", "a", "e", "e", "e", "e", "e"
+                , "e", "e", "e", "i", "i", "i", "i", "i", "i"
+                , "i", "i", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o"
+                , "u", "u", "u", "u", "u", "u", "u", "u"
+            };
+            for (int i = 0; i < 79; i++){
+                while (true){
+                    string::size_type achada = palavra.find(errado[i]);
+                    if (achada != string::npos){
+                        palavra.replace(achada, errado[i].length(), certo[i]);
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            for(char x: palavra){
+                if(x > 96 && x < 123){
+                    resultado.push_back(x);
+                }
+            }
+            return resultado;
+        }
+    //* Normaliza as palavras.
         map<string, set<string>> indice_;
 };
