63103bf95c7b045994bea223f08007f97c04889d
rbmuri <murilo.r.freitas@hotmail.com>
2022-11-27 04:47:45 -0300

    comecei a escrever o código principal organizei mais pastas

--- include/indexacao.h
+++ include/indexacao.h
@@ -1,11 +1,28 @@
 #include <map>
 #include <set>
-#include <iostream>
+#include <string>
+#include <vector>
 
-class Indexacao {
+using namespace std;
+
+class Indice {
     public:
-        
+        void indexar(string doc, string nome){
+            int i;
+            vector<string> doclista = separar(doc);
+            for (i=0; i<doclista.size(); i++){
+                indice_[doclista[i]].insert(nome); 
+            }
+        }
     
     private:
+        vector<string> separar(string doc);
+            //* codigo do lucas
+        
+
+        string normalizar(string palavra);
+            //* codigo do lucas
+        
+
         std::map<string, std::set<string>> indice_;
-}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <indexacao.h>
+#include <filesystem>
 
 using namespace std;
 
 int main()
 {
+    
     cout<<"maquina de busca\n";
     return 0;
 }
